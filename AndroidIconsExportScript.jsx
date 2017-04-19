f4e0e84f14af2331238227c21779459e05404ce8
Claffolo <claudioclaff@gmail.com>
2017-04-19 12:22:38 +0200

    Ui Editing
    - Added PNG24 Options Panel
    - Added Folder Type Panel (drawable and mipmap)
    - Added the Scale Factor value to the name of the Scale Factors

--- AndroidIconsExportScript.jsx
+++ AndroidIconsExportScript.jsx
@@ -3,120 +3,187 @@
 *   TODO: Add Licensing
 *
 */
+try{
 
-var selectedExportOptions = {};
+  //Check if there are open documents (if not throw an error and exit)
+  if(app.documents.length > 0){
 
-var androidExportOptions = [
-     {
+    //The user choose the export folder
+    var folder = Folder.selectDialog("Choose your export folder");
+    var document = app.activeDocument; //The current open and active document
+
+    var selectedExportOptions = {}; //Array used to store all the elements of the checklist that the user selected
+
+    //Array containing all the different scale factors defined in the Google Android Developers guidelines
+    var androidExportOptions = [
+      {
         name: "ldpi (0.75x)",
         scaleFactor: 75,
-    },
-    {
+      },
+      {
         name: "mdpi (1.0x)",
         scaleFactor: 100,
-    },
-    {
+      },
+      {
         name: "hdpi (1.5x)",
         scaleFactor: 150,
-    },
-    {
+      },
+      {
         name: "xhdpi (2.0x)",
         scaleFactor: 200,
-    },
-    {
+      },
+      {
         name: "xxhdpi (3.0x)",
         scaleFactor: 300,
-    },
-    {
+      },
+      {
         name: "xxxhdpi (4.0x)",
         scaleFactor: 400,
-    }
-];
-
-var folder = Folder.selectDialog("Choose your export folder");
-var document = app.activeDocument;
-
-if(document && folder) {
-    var dialog = new Window("dialog","Select the desired export sizes");
-    var osGroup = dialog.add("group");
-
-    var androidCheckboxes = createSelectionPanel("Scale Factors", androidExportOptions, osGroup);
-
-    var exportFormatPanel = osGroup.add("panel", undefined, "Export Format");
-    exportFormatPanel.alignment = "top";
-    var formatDropDown = exportFormatPanel.add("dropdownlist");
-    formatDropDown.add ("item", "PNG24");
-    formatDropDown.add ("item", "PNG8");
+      }
+    ];
 
 
+    if(document && folder) {
+      var dialog = new Window("dialog","Select the desired export sizes");
+      var uiGroup = dialog.add("group");
 
-    var buttonGroup = dialog.add("group");
-    var okButton = buttonGroup.add("button", undefined, "Export");
-    var cancelButton = buttonGroup.add("button", undefined, "Cancel");
+      var androidCheckboxes = createSelectionPanel("Scale Factors", androidExportOptions, uiGroup);
 
-    okButton.onClick = function() {
+      var PNG24Options = new ExportOptionsPNG24();
+
+      //Creating the ExportOptions Panel
+      var exportFormatPanel = uiGroup.add("panel", undefined, "PNG24 Options");
+      exportFormatPanel.alignment = "top";
+      exportFormatPanel.alignChildren = "left";
+      var transparency_checkbox = exportFormatPanel.add("checkbox", undefined, "Include Transparency");
+      var antiAliasing_checkbox = exportFormatPanel.add("checkbox", undefined, "Anti-Aliasing");
+
+      //Defining the onClick methods of the PNG24Options checkboxes
+      transparency_checkbox.onClick = function(){
+        if(this.value){
+          PNG24Options.transparency = true;
+        }
+        else{
+          PNG24Options.transparency = false;
+        }
+      };
+
+      antiAliasing_checkbox.onClick = function(){
+        if(this.value){
+          PNG24Options.antiAliasing = true;
+        }
+        else{
+          PNG24Options.antiAliasing = false;
+        }
+      };
+
+      //Creating the FolderType Panel
+
+      var folderType = "drawable";
+
+      var folderTypePanel = uiGroup.add("panel", undefined, "Folder type");
+      folderTypePanel.alignment = "top";
+      folderTypePanel.alignChildren = "left";
+      var drawable_radiobutton = folderTypePanel.add ("radiobutton", undefined, "drawable");
+      var mimap_radiobutton = folderTypePanel.add ("radiobutton", undefined, "mipmap");
+
+      drawable_radiobutton.onClick = function(){
+        if(this.value){
+          folderType = "drawable";
+        }
+      };
+      mipmap_radiobutton.onClick = function(){
+        if(this.value){
+          folderType = "mipmap";
+        }
+      };
+
+
+
+      var buttonGroup = dialog.add("group");
+      var okButton = buttonGroup.add("button", undefined, "Export");
+      var cancelButton = buttonGroup.add("button", undefined, "Cancel");
+
+      okButton.onClick = function() {
         for (var key in selectedExportOptions) {
-            if (selectedExportOptions.hasOwnProperty(key)) {
-                var item = selectedExportOptions[key];
-                exportToFile(item.scaleFactor, item.name, item.type);
-            }
+          if (selectedExportOptions.hasOwnProperty(key)) {
+            var item = selectedExportOptions[key];
+            exportToFile(item.scaleFactor, item.name, item.type);
+          }
         }
         this.parent.parent.close();
-    };
+      };
 
-    cancelButton.onClick = function () {
+      cancelButton.onClick = function () {
         this.parent.parent.close();
-    };
+      };
 
-    dialog.show();
+      dialog.show();
+    }
+
+  }
+
+  else{
+    throw new Error ("You have to open an existing document or create a new one!");
+  }
+}
+
+catch(error){
+  alert (error.message, "Script Error");
 }
 
 function exportToFile(scaleFactor, resIdentifier, os) {
-    var i, ab, file, options, expFolder;
+  //TODO: Rewrite this function
+  var i, ab, file, options, expFolder;
+  if(os === "android")
+  expFolder = new Folder(folder.fsName + "/drawable-" + resIdentifier);
+
+  if (!expFolder.exists) {
+    expFolder.create();
+  }
+
+  for (i = document.artboards.length - 1; i >= 0; i--) {
+    document.artboards.setActiveArtboardIndex(i);
+    ab = document.artboards[i];
+
+    if(ab.name.charAt(0)=="!")
+    continue;
+
     if(os === "android")
-        expFolder = new Folder(folder.fsName + "/drawable-" + resIdentifier);
+    file = new File(expFolder.fsName + "/" + ab.name + ".png");
+    else if(os === "ios")
+    file = new File(expFolder.fsName + "/" + ab.name + resIdentifier + ".png");
 
-	if (!expFolder.exists) {
-		expFolder.create();
-	}
+    options = new ExportOptionsPNG24();
+    options.transparency = true;
+    options.artBoardClipping = true;
+    options.antiAliasing = true;
+    options.verticalScale = scaleFactor;
+    options.horizontalScale = scaleFactor;
 
-	for (i = document.artboards.length - 1; i >= 0; i--) {
-		document.artboards.setActiveArtboardIndex(i);
-		ab = document.artboards[i];
-
-	if(ab.name.charAt(0)=="!")
-            continue;
-
-        if(os === "android")
-            file = new File(expFolder.fsName + "/" + ab.name + ".png");
-        else if(os === "ios")
-            file = new File(expFolder.fsName + "/" + ab.name + resIdentifier + ".png");
-
-            options = new ExportOptionsPNG24();
-            options.transparency = true;
-            options.artBoardClipping = true;
-            options.antiAliasing = true;
-            options.verticalScale = scaleFactor;
-            options.horizontalScale = scaleFactor;
-
-            document.exportFile(file, ExportType.PNG24, options);
-	}
+    document.exportFile(file, ExportType.PNG24, options);
+  }
 };
 
-function createSelectionPanel(name, array, parent) {
-    var panel = parent.add("panel", undefined, name);
-    panel.alignChildren = "left";
-    for(var i = 0; i < array.length;  i++) {
-        var cb = panel.add("checkbox", undefined, "\u00A0" + array[i].name);
-        cb.item = array[i];
-        cb.onClick = function() {
-            if(this.value) {
-                selectedExportOptions[this.item.name] = this.item;
-                //alert("added " + this.item.name);
-            } else {
-                delete selectedExportOptions[this.item.name];
-                //alert("deleted " + this.item.name);
-            }
-        };
-    }
+//Function that creates the android export sizes checkbox panel, based on the array defined at the beginning of the script
+function createSelectionPanel(name, array, group) {
+
+  var panel = group.add("panel", undefined, name);
+  panel.alignment = "left";
+  panel.alignChildren = "left";
+
+  for(var i = 0; i < array.length;  i++) {
+
+    var cb = panel.add("checkbox", undefined, "\u00A0" + array[i].name);
+    cb.item = array[i];
+
+    //The OnClick function of each checkbox adds (or removes) the selected element from the selected options array
+    cb.onClick = function() {
+      if(this.value) {
+        selectedExportOptions[this.item.name] = this.item;
+      } else {
+        delete selectedExportOptions[this.item.name];
+      }
+    };
+  }
 };
